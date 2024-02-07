b59b6e157a4d932bb98b28adc05d52b8b411d489
ryanharrishtx@gmail.com <ryanharrishtx@gmail.com>
2024-02-06 19:25:36 -0600

    starting to add chat functionality

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
 const express  = require('express');
 const dotenv = require('dotenv');
-
-const app = express();
+const connectDB = require('./config/db');
+const colors = require ('colors');
+const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/user', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3002;
 
-app.get('/', (req, res) => {
-    res.send("API is running successfully");
-});
+app.use('/api/user', userRoutes);
 
-
-app.listen(3002, console.log(`Server running on port ${PORT}`));
+app.listen(3002, console.log(`Server running on port ${PORT}`.yellow.bold));
